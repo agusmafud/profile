887775c78a8b748082953be37163df7fbc7587aa
Agustín Mafud <agusmafud@gmail.com>
2022-12-12 15:54:39 -0300

    new buid version

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   HStack,
   Center,
@@ -12,6 +12,57 @@ import { Skill as SkillType } from 'types';
 const skillsPalette = ['green', 'teal', 'blue', 'cyan', 'purple', 'pink'];
 const getSkillColor = (index: number) => skillsPalette[index % skillsPalette.length];
 
+const Skill = ({
+  skill,
+  index,
+} : {
+  skill: SkillType,
+  index: number,
+}) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const handleTooltipClick = () => setShowTooltip((prevState) => !prevState);
+  const SkillIcon = skill.Icon ?? null;
+
+  return (
+    <Center
+      key={skill.label}
+      cursor="pointer"
+      flexWrap="wrap"
+      padding={3}
+      paddingTop={0}
+      paddingLeft={0}
+    >
+      <Tooltip
+        isOpen={showTooltip}
+        label={(
+          <>
+            {SkillIcon}
+            <Text>{skill.description}</Text>
+          </>
+        )}
+        hasArrow
+        placement="top"
+        background="white"
+        color="gray.900"
+      >
+        <Badge
+          key={skill.label}
+          variant="subtle"
+          fontSize="0.8em"
+          colorScheme={getSkillColor(index)}
+          transition="all 300ms"
+          _hover={{
+            transform: 'scale(1.1)',
+          }}
+          onClick={handleTooltipClick}
+        >
+          {skill.label}
+        </Badge>
+      </Tooltip>
+    </Center>
+  );
+};
+
 const Skills = ({
   skills,
   index,
@@ -21,45 +72,7 @@ const Skills = ({
     wrap="wrap"
     marginTop={3}
   >
-    {skills.map((skill) => {
-      const SkillIcon = skill.Icon ?? null;
-      return (
-        <Center
-          key={skill.label}
-          cursor="pointer"
-          flexWrap="wrap"
-          padding={3}
-          paddingTop={0}
-          paddingLeft={0}
-        >
-          <Tooltip
-            label={(
-              <>
-                {SkillIcon}
-                <Text>{skill.description}</Text>
-              </>
-            )}
-            hasArrow
-            placement="top"
-            background="white"
-            color="gray.900"
-          >
-            <Badge
-              key={skill.label}
-              variant="subtle"
-              fontSize="0.8em"
-              colorScheme={getSkillColor(index)}
-              transition="all 300ms"
-              _hover={{
-                transform: 'scale(1.1)',
-              }}
-            >
-              {skill.label}
-            </Badge>
-          </Tooltip>
-        </Center>
-      );
-    })}
+    {skills.map((skill) => <Skill skill={skill} index={index} />)}
   </HStack>
 );
 
